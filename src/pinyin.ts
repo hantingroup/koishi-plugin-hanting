ca356b37574cfd544f842d205dee728734493960
montmorill <montmorill@outlook.com>
2026-06-27 01:26:22 +0800

    Bumps version to 1.1.1 and updates pinyin masking functionality to replace original words with pinyin in sentences

--- src/pinyin.ts
+++ src/pinyin.ts
@@ -5,8 +5,19 @@ const pinyinSeparator = /[- ]/
 type Pinyin = (sentence: string) => { origin: string, pinyin: string }[]
 
 export function maskPinyin(hanting: Tables['hanting'], pinyin: Pinyin): void {
+  const replaceMap = new Map()
+  const words = hanting.word.split('/')
+  let index = 0
+  for (const pinyin of hanting.pinyin.split(pinyinSeparator)) {
+    for (const word of words)
+      replaceMap.set(word[index], pinyin)
+    index++
+  }
+
   const pinyinSet = new Set(hanting.pinyin.toLowerCase().split(pinyinSeparator))
   const maskText = (sentence: string) => {
+    for (const [origin, pinyin] of replaceMap)
+      sentence = sentence.replaceAll(origin, pinyin)
     return pinyin(sentence)
       .map(({ origin, pinyin }) => hanting.word.includes(origin)
         || pinyinSet.has(pinyin) ? ` ${pinyin} ` : origin)
